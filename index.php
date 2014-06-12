a711a00236bdfdf18151452cc4451425c72b73ae
MatthieuHarbich <matthieu.harbich@gmail.com>
2014-06-12 15:59:51 +0200

    Squelette

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-dsafdsfsdfs
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+	   <title>ZED</title>
+	   <meta charset="utf-8">
+	   <link rel="stylesheet" type="text/css" href="css/main.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script src="js/jquery.js" type="text/javascript"></script>
+        <script src="js/modernizr.js" type="text/javascript"></script>
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        Hello!
+    </body>
+</html>
