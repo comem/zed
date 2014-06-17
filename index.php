b75b1b1536177a0bea3b042cfda38684e755a996
MatthieuHarbich <matthieu.harbich@gmail.com>
2014-06-17 08:53:40 +0200

    mise à jour

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <div  id="searchToBeSwitched" class="normalSearch">
                  <input id="searchInput" type="text" placeholder="search">
             </div>           
-          <div> yeaaag</div> 
+          
            
            <!--  <div id="minimizedSearchSection" >
                 <div id="minimizedSearch">
