bd7ab76e67b413796954841c1b3be9188407f482
MatthieuHarbich <matthieu.harbich@gmail.com>
2014-06-25 17:53:43 +0200

    jlajd

--- js/main.js
+++ js/main.js
@@ -122,6 +122,26 @@ var EventAdd = MyView.extend({
     },
     showUp: function(event){
         console.log('showUp');
+        /* This is basic - uses default settings */
+    
+        $("a#single_image").fancybox();
+        
+        /* Using custom settings */
+        
+        $("a#inline").fancybox({
+            'hideOnContentClick': true
+        });
+
+        /* Apply fancybox to multiple items */
+        
+        $("#eventImgBrowse").fancybox({
+            'transitionIn'  :   'elastic',
+            'transitionOut' :   'elastic',
+            'speedIn'       :   600, 
+            'speedOut'      :   200, 
+            'overlayShow'   :   false
+        });
+    
     }
 });
 
