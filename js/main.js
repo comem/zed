70a939b74be08c24beb9a69d7459ad872fb3d3c4
ginold <ginold@hotmail.com>
2014-07-01 18:01:22 +0200

    merge top with adam

--- js/main.js
+++ js/main.js
@@ -884,34 +884,6 @@ $('.myAccordion').accordion({collapsible: true, active: false,heightStyle: "cont
         $('.app').html(modalView.render().el);
 
 
-        var ImageView = Backbone.View.extend({
-            initialize: function(attrs, options){
-                 this.listenTo(this.model, 'all', this.render);
-                 //this.listenTo(this.model, 'remove', this.logDelete);
-                 //this.listenTo(this.model, 'add' ,this.logAdd)
-            },
-           template : _.template(templates.lists_listImage),
-            render: function() {
-                this.$el.html(this.template(this.model.toJSON()));
-                return this;
-            }
-            
-        });
-
-
-var imageView = new ImageView({model:imagescollnested})
-imagescollnested.get("images").fetch({
-  success:function () {
-    imageView.render().$el.appendTo("#selectable");
-    console.log(imagescollnested.toJSON())
-
-  }
-})
-
-      });
-
-     
-
 
 
 
