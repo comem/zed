df93c27a6f1201f17f5f07e6b9e343e4738f2063
richard <ammannrichard@gmail.com>
2014-07-02 11:01:50 +0200

    login

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-	   <title>ZED</title>
-	   <meta charset="utf-8">
-	   <link rel="stylesheet" type="text/css" href="css/style.css">        
+       <title>ZED</title>
+       <meta charset="utf-8">
+       <link rel="stylesheet" type="text/css" href="css/style.css">        
         <script src="js/jquery.js" type="text/javascript"></script>
         <script src="js/jquery-ui.js" type="text/javascript"></script>     
 
@@ -11,6 +11,29 @@
         <script src="js/main.js"></script>
     </head>
     <body id="login">
-       <img id="logoLogin" src="img/logo.png">
+
+        <div class="header">
+             <img id="logoLogin" src="img/logo.png">
+        </div>
+
+       <div class="formulaire">
+            
+            <form id ="login_form" action="index.php">
+                <h1>Login</h1>
+                <h2>to continue</h2>
+                <div class="input">
+                    <div class="blockinput">
+                    <input type="text" placeholder="Username"> <br>
+                     <input type="password" placeholder="Password">
+                    </div>
+                    <div class="roundedTwo">
+                        <input class="css-checkbox" type="checkbox" value="" id="remember" name=""/>
+                        <label class="css-label"for="remember">Remember me</label>
+                    <div/>
+                </div>
+                <button>Login</button>
+            </form>
+
+       </div>
     </body>
 </html>
